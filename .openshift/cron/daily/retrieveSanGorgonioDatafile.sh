28dabcbb0d00a538d4bf802f433570f3907cf2aa
darren@osten.net <darren@osten.net>
2015-02-22 20:30:32 -0800

    moved SanG retrieve script to daily

--- .openshift/cron/daily/retrieveSanGorgonioDatafile.sh
+++ .openshift/cron/daily/retrieveSanGorgonioDatafile.sh
@@ -4,3 +4,4 @@ od=$OPENSHIFT_DATA_DIR
 echo "od=$od"
 cd $od
 wget -o $od/sang.log http://www.howlingduck.com/triterra/gorgonio/datafile.txt
+rm -f datafile.txt.*
--- .openshift/cron/minutely/retrieveSanGorgonioDatafile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-od=$OPENSHIFT_DATA_DIR
-echo "od=$od"
-cd $od
-wget -o $od/sang.log http://www.howlingduck.com/triterra/gorgonio/datafile.txt
