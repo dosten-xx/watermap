a008cb7ba486134a4b11edb8057a760d38b9e114
darren@osten.net <darren@osten.net>
2015-03-05 20:44:01 -0800

    fixed scripts for linux

--- .openshift/cron/daily/retrievePCTFiles.sh
+++ .openshift/cron/daily/retrievePCTFiles.sh
@@ -3,7 +3,6 @@
 od=$OPENSHIFT_DATA_DIR
 echo "od=$od"
 cd $od
-curl -sk -o $od/pct-a.htm https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc&gid=0
-#wget https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc&gid=0
-curl -sk -o $od/pct-c.htm https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc&gid=2
-curl -sk -o $od/pct-e.htm https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc&gid=3
+wget -O $od/pct-a.htm https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc&gid=0
+wget -O $od/pct-c.htm https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc&gid=2
+wget -O $od/pct-e.htm https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc&gid=3
