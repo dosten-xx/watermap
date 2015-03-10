24e06c477242bc60312b81e48de55effbd1277b0
darren@osten.net <darren@osten.net>
2015-03-09 17:23:01 -0700

    another try to fix the pct downloads

--- .openshift/cron/daily/retrievePCTFiles.sh
+++ .openshift/cron/daily/retrievePCTFiles.sh
@@ -3,6 +3,6 @@
 od=$OPENSHIFT_DATA_DIR
 echo "od=$od"
 cd $od
-wget -O $od/pct-a.htm https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc&gid=0
-wget -O $od/pct-c.htm https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc&gid=2
-wget -O $od/pct-e.htm https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc&gid=3
+curl -o $od/pct-a.htm -G -d gid=0 https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc
+curl -o $od/pct-c.htm -G -d gid=2 https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc
+curl -o $od/pct-e.htm -G -d gid=3 https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc
