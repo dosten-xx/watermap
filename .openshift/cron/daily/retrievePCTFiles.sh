ed7e13a0ab463e2ed225320032bd9cce1b70e1d3
darren@osten.net <darren@osten.net>
2015-03-16 18:26:27 -0700

    updated case in PCT script

--- .openshift/cron/daily/retrievePCTFiles.sh
+++ .openshift/cron/daily/retrievePCTFiles.sh
@@ -3,6 +3,6 @@
 od=$OPENSHIFT_DATA_DIR
 echo "od=$od"
 cd $od
-curl -o $od/pct-a.htm -G -d gid=0 https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc
-curl -o $od/pct-c.htm -G -d gid=2 https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc
-curl -o $od/pct-e.htm -G -d gid=3 https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc
+curl -o $od/pct-A.htm -G -d gid=0 https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc
+curl -o $od/pct-C.htm -G -d gid=2 https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc
+curl -o $od/pct-E.htm -G -d gid=3 https://docs.google.com/spreadsheet/pub?key=0AnjydhFdh1E2dEtQWEFXOGVHeWtsQVlCSnFXcTh2VXc
