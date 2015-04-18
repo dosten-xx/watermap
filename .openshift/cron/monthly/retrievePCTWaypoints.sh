2ecba83d1766c8ccd556180d388ceec553e19057
darren@osten.net <darren@osten.net>
2015-04-17 19:19:52 -0700

    PCT waypoints get NoCal sections

--- .openshift/cron/monthly/retrievePCTWaypoints.sh
+++ .openshift/cron/monthly/retrievePCTWaypoints.sh
@@ -38,4 +38,34 @@ unzip -o ca_section_g_gps.zip
 mv ca_section_g_gps/* .
 rm -rf ca_section_g_gps
 
+wget http://www.pctmap.net/pctdownloads/ca_section_m_gps.zip
+unzip -o ca_section_m_gps.zip
+mv ca_section_m_gps/* .
+rm -rf ca_section_m_gps
+
+wget http://www.pctmap.net/pctdownloads/ca_section_n_gps.zip
+unzip -o ca_section_n_gps.zip
+mv ca_section_n_gps/* .
+rm -rf ca_section_n_gps
+
+wget http://www.pctmap.net/pctdownloads/ca_section_o_gps.zip
+unzip -o ca_section_o_gps.zip
+mv ca_section_o_gps/* .
+rm -rf ca_section_o_gps
+
+wget http://www.pctmap.net/pctdownloads/ca_section_p_gps.zip
+unzip -o ca_section_p_gps.zip
+mv ca_section_p_gps/* .
+rm -rf ca_section_p_gps
+
+wget http://www.pctmap.net/pctdownloads/ca_section_q_gps.zip
+unzip -o ca_section_q_gps.zip
+mv ca_section_q_gps/* .
+rm -rf ca_section_q_gps
+
+wget http://www.pctmap.net/pctdownloads/ca_section_r_gps.zip
+unzip -o ca_section_r_gps.zip
+mv ca_section_r_gps/* .
+rm -rf ca_section_r_gps
+
 rm ca_section_*_gps.zip.*
